57418bd7542880395f82c1bd94f56fc79522efcc
Bryan Ramos <b.r.tech95@gmail.com>
2019-07-03 16:35:19 -0400

    Migrated from gulp 3.9 to gulp 4.0

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -10,37 +10,38 @@
  * gulp-minify-css - minifies the CSS file for faster rendering.
  */
 
-let gulp    = require('gulp'),
+let gulp = require('gulp'),
  imagemin   = require('gulp-imagemin'),
  autoprefix = require('gulp-autoprefixer'),
  concat     = require('gulp-concat'),
  cssMin     = require('gulp-minify-css');
 
-// gulp.task('imagemin', function() {
-//     var img_src = './src/images/**/*', img_dest = './build/images';
- 
-//     gulp.src(img_src)
-//     .pipe(imagemin())
-//     .pipe(gulp.dest(img_dest));
-//  });
-
-let cssDir = './src/styles/*.css';
-let cssBuildDir = './build/styles/';
-
-
- gulp.task('imagemin', () =>
+ function done()
  {
+    console.log("Task complete");
+    return;
+ }
+ 
+function minimizeImages(done){
     var img_src = './src/images/**/*', img_dest = './build/images';
-	return gulp.src(img_src)
+	gulp.src(img_src)
 		.pipe(imagemin())
         .pipe(gulp.dest(img_dest))
- }
-);
+    done();
+ };
 
-gulp.task('styles', () => {
-    return gulp.src([cssDir])
+function styles(done){
+    let cssDir = './src/styles/*.css';
+    let cssBuildDir = './build/styles/';
+    gulp.src([cssDir])
     .pipe(concat('styles.css'))
     .pipe(autoprefix('last 2 versions'))
     .pipe(cssMin())
     .pipe(gulp.dest(cssBuildDir));
-});
+    done();
+};
+
+
+gulp.task('default',gulp.series(minimizeImages, styles));
+
+//  exports.default = defaultTask
