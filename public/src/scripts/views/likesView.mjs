e6dc6fb6aecf1fbce9d03faa4a60407a95f9ec47
Bryan Ramos <b.r.tech95@gmail.com>
2019-07-21 18:32:52 -0400

    Fixed the delete feature so the DOM child elements are properly deleted without any garbage elements being left behind

--- public/src/scripts/views/likesView.mjs
+++ public/src/scripts/views/likesView.mjs
@@ -3,7 +3,7 @@ import {alternateMode, isDark} from './changeMode.mjs';
 
 import {toggleClosingButton} from './toggle.mjs'
 
-
+let indexCounter = 0;
 //Toggle the 'save' button 
 export const toggleLike = () => {
    
@@ -40,26 +40,27 @@ export const toggleLikeMenu = () => {
 export const renderLike = like => {
     
     let markup = `
-			<li class="likedElement">
-                <a class="likes__link" href="#${like.id}">
-                    <div class="info-container ${isDark()}">
-                        <h1 class="lightblue"><a href="" class="lightblue">${like.name}</a></h1>
-                        <h2>Address: ${like.address}</h2>
+    <li class="likedElement" id="element_${like.id}">
+        <a class="likes__link" href="#${like.id}">
+            <div class="info-container ${isDark()}">
+                <h1 class="lightblue"><a href="" class="lightblue">${like.name}</a></h1>
+                    <h2>Address: ${like.address}</h2>
                         <h3>Phone#: ${like.phone}</h3>
                         <h3 class="lightblue"><a href=""class="lightblue">Get Directions</a></h3>
-                        <a class="delete__brewery" id="brewery__${like.id}" style="color: #ff4444" href="#${like.id}">Delete</a>
-                    </div>
-                </a>
-            </li>
+            <a class="delete__brewery" id="brewery__${like.id}" style="color: #ff4444" href="#${like.id}">Delete</a>
+            </div>
+        </a>
+    </li>
     `;
     elements.likeList.insertAdjacentHTML('beforeend', markup);
+    indexCounter += 1;
 }
 
 export const deleteLike = id =>
 {
     // grab child element from parent.
-    const el = document.querySelector(`#brewery__${id}`);
-    if(el) el.parentNode.remove();
+    const el = document.querySelector(`#element_${id}`);
+    if(el) el.parentElement.removeChild(el);
     //Remove hash from URL
     history.replaceState(null,null,' ');
 }
